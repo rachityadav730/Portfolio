29a8b3da914def40c7be45839213e117fbc44292
rachityadav730 <rachit.r.yadav@gmail.com>
2024-02-05 23:37:59 +0530

    home page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
+import ScrollToTop from './components/ScrollToTop';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+import Home from './pages/Home';
+// import Projects from './pages/Projects';
+// import Footer from './components/Footer';
 
 function App() {
   return (
    <>
    <Router>
         <NavMenu />
+        <ScrollToTop />
+        <Routes>
+          {/* <Route path="/about"  element={<About />} >
+            
+          </Route>
+          <Route path="/contact"  element={<Contact />} >
+            
+          </Route>
+          <Route path="/projects"  element={<Projects />} > 
+            
+          </Route> */}
+          <Route path="/"  element={<Home />} />
+            
+        
+        </Routes>
+        {/* <Footer /> */}
     </Router>
    </>
   );
---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,11 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Nikhil',
+    title: 'Senior Software Engineer',
+    org: 'Setlmint',
+    desc:
+      'Rachit is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+  },
+];
+export default testimonials;
